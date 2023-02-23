45bc4b24890c2f3d2af822ba0582b2e2272df300
grovergonzalez <grover.gonzalez@ucb.edu.bo>
2023-02-23 19:56:16 -0400

    Seleccionar estado y mostrar eleccion

--- src/presenter.js
+++ src/presenter.js
@@ -1,13 +1,16 @@
 
 const cantItems = document.querySelector("#Cant-Item");
 const precioItems = document.querySelector("#Precio-Item");
+const codEstados = document.querySelector("#estados")
 const form = document.querySelector("#Mostrar-form");
 const Cantdiv = document.querySelector("#Cant-div");
 const preciodiv = document.querySelector("#Precio-div");
+const codEstadosDiv = document.querySelector("#CodEstados-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   Cantdiv.innerHTML = "Cantidad de Items: " + cantItems.value ;
   preciodiv.innerHTML = "Precio por Item: " + precioItems.value;
+  codEstadosDiv.innerHTML = "Codigo de Estado: " + codEstados.value;
 });
