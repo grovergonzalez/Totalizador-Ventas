1f10ccdc55c8ddbaa346ae383c4528ff6fcf00fb
grovergonzalez <grover.gonzalez@ucb.edu.bo>
2023-02-23 19:48:28 -0400

    Ingresar el precio por item y mostrar en pantalla

--- src/presenter.js
+++ src/presenter.js
@@ -1,10 +1,13 @@
 
 const cantItems = document.querySelector("#Cant-Item");
+const precioItems = document.querySelector("#Precio-Item");
 const form = document.querySelector("#Mostrar-form");
-const div = document.querySelector("#Total-div");
+const Cantdiv = document.querySelector("#Cant-div");
+const preciodiv = document.querySelector("#Precio-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  div.innerHTML = "Cantidad de Items: " + cantItems.value ;
+  Cantdiv.innerHTML = "Cantidad de Items: " + cantItems.value ;
+  preciodiv.innerHTML = "Precio por Item: " + precioItems.value;
 });
